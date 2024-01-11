862ed848f64e77e17e061125a2a3eb74b938c51a
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2024-01-11 17:11:03 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <main className="mx-auto">
+      <Home />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <section>
+      <Navbar />
+    </section>
+  );
+};
+
+export default Home;
